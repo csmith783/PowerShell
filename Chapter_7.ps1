0295bac4934e607093b34a7bd0de04026086b67e
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-08 10:58:39 -0500

    Chapter 7 Solution
    Exploring PSSnapins, PSModules, and PSProviders

---
+++ Chapter_7.ps1
@@ -0,0 +1,12 @@
+﻿# Run the networking troubleshooting pack, pass in an instance ID, run a web connectivity check and ask for help connecting to a specific web page. 
+help *troubleshooting*
+Import-Module -Name TroubleshootingPack
+man Invoke-TroubleshootingPack -ShowWindow
+man Get-TroubleshootingPack -ShowWindow
+dir "C:\Windows\diagnostics\system"
+$tpack = Get-TroubleshootingPack -Path "C:\Windows\diagnostics\system\Networking"
+Invoke-TroubleshootingPack $tpack
+# Enter; leaving the Instance ID blank
+# 1; for Wb Connectivity
+# 2; to connect to a specific site
+# https://www.google.com
