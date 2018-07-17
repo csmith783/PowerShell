6c998c9d966c0a840a7975f2a60c5d25b87aa79c
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-16 20:26:46 -0500

    Chapter 13 Solutions
    Remoting; Enter-PSSession, Invoke-Command

---
+++ CHapter_13.ps1
@@ -0,0 +1,16 @@
+﻿# Make a 1-1 connection witha a remote computer (localhost); launch notepad.exe
+Enter-PSSession -ComputerName localhost
+notepad.exe
+
+# Using invoke-command, retreive a list of services that are not started from one or two remote computers. format the results in a wide list 
+Invoke-Command -ComputerName localhost, localhost -ScriptBlock { Get-Service | ? { $_.Status -eq "Stopped" }} | fw
+
+# Use invoke-command to get a list of the top 10 processes for virtual memory usage. target 1-2 computers. 
+Invoke-Command -ComputerName localhost, localhost -ScriptBlock { Get-Process | sort VirtualMemorySize -Descending | select -First 10 }
+
+# Create a text file taht contains three computer names with one name per line. use invoke-command to retreive the 100 newest application event logs from them 
+"localhost", "localhost", "localhost" | Out-File Servers.txt
+Invoke-Command -ComputerName (cat .\Servers.txt) -ScriptBlock { Get-EventLog -LogName Application -Newest 100 } 
+
+# Using invoke-command, query 1 or more remote servers to display the properties Product name, editionID, and Current version from the registry key 'HKEY_Local_Machine\Software\Microsft\Windows NT\CurrentVersion\
+Invoke-Command -ComputerName localhost -ScriptBlock { Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'} | Select-Object ProductName, EditionID, CurrentVersion
