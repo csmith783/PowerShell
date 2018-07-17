478629bd38955a49969861e9569e088c1084e6f8
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-17 07:07:35 -0500

    Chapter 17 Solutions
    Setting the execution policy to allow script execution.

---
+++ Chapter_17.ps1
@@ -0,0 +1,3 @@
+﻿# Execute the necessary commands to configure script execution on your machine. We suggest using RemoteSigned. 
+# Must be ran as an administrator in order to write to the HKLM portion of the registry 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
