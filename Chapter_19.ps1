3e709299d05f4180ba5617dd91b80428903f1c12
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-19 13:04:52 -0500

    Chapter 19 Solutions
    Reading and writing output to the console

---
+++ Chapter_19.ps1
@@ -0,0 +1,12 @@
+﻿# Use write-output to display the results of 100 * 10
+Write-Output (100*10)
+
+# Use write-host to display the results of 100 * 10
+Write-Host (100*10)
+
+# prompt the user to enter a name and then display the name in yellow text
+$name = Read-Host "Please enter your name" 
+Write-Host $name -ForegroundColor Yellow
+
+# Prompt the user to enter a name, and then display the name only if its longer than 5 charachters. do this with a single powershell expression, don't use a variable
+Read-Host "Please enter your name"  | where { $_.Length -gt 5 }
