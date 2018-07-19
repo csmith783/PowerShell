67970f47f0b79b5f72e1c442f213462a6d31e73a
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-19 07:05:44 -0500

    Chapter 18 Solutions
    Working with Variables

---
+++ Chapter_18.ps1
@@ -0,0 +1,12 @@
+﻿# Create a background job that querries the win32_bios information from 2 computers 
+Invoke-Command -ComputerName localhost, localhost -ScriptBlock { Get-WmiObject -Class Win32_BIOS } -AsJob
+
+# Receive the results of the job into a variable
+get-job
+$results = Receive-Job -id 2
+
+# Display the contents of that variable
+$results
+
+# Export the variables contents to a cliXML file
+$resulte | Export-Clixml win32_bios.xml
