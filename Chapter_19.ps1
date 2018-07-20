80f7751cb8679d7632bfd27340b35f8a0db4f2de
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-20 06:49:42 -0500

    Update Chapter_19.ps1

--- Chapter_19.ps1
+++ Chapter_19.ps1
@@ -10,3 +10,4 @@ Write-Host $name -ForegroundColor Yellow
 
 # Prompt the user to enter a name, and then display the name only if its longer than 5 charachters. do this with a single powershell expression, don't use a variable
 Read-Host "Please enter your name"  | where { $_.Length -gt 5 }
+Write-Output (Read-Host "what") | where { $_.length -gt 5 }
