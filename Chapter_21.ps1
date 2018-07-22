898a9f81d9ce4b2b63df2d6299fa5f22430dfd57
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-07-21 19:02:47 -0500

    Chapter 21 Solutions
    Creating a script, script parameters, creating a help file for the script.

---
+++ Chapter_21.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.Synopsis
+Gets drives based on percentage of free space.
+.Description
+This command will get all local drives that have less than the specified
+    percentage of free space
+.Parameter ComputerName
+The name of the computer to check
+.Parameter DriveType
+THe drive type to check
+.Parameter MinimumPercentFree
+The minimum percent free disk space. This is the threshold. The default value is 10.
+    Enter a number between 1 and 100. 
+.Example
+PS C:\> Get-Disk -MinimumPercentFree 20
+
+#>
+
+
+param (
+    $computername = $env:COMPUTERNAME,
+    $driver = 3,
+    $MinimumPercentFree = 10
+)
+
+Get-WmiObject Win32_LogicalDisk -filter "drivetype=$driver" |
+    Where { ($_.FreeSpace / $_.Size) -lt $MinimumPercentFree } |
+    Select -Property DeviceID, FreeSpace, Size
