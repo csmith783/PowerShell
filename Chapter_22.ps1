bba2cc82773242e27955ea404f9c5b03e50aab25
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-08-02 07:36:50 -0500

    Chapter 22 Solutions
    Parameterized Script, verbose, mandatory parameters, aliases, and more.

---
+++ Chapter_22.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+Gets physical network adapters.
+.Description
+This command will get all physical Network Adapters from the LocalHost
+.Parameter ComputerName
+The name of the computer to check
+.Example
+PS C:\> ./chapter_22.ps1 -computername localhost
+
+#>
+[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory=$true)]
+    [Alias('hostname')]
+    [string]$computername
+)
+
+Write-Verbose "Getting Physical Network Adapter for computer $computername."
+Get-WmiObject win32_networkadapter -ComputerName $computername |
+    where { $_.PhysicalAdapter } |
+    select MACAddress, AdapterType, DeviceID, Name, Speed
+Write-Verbose "Finishing Script."
