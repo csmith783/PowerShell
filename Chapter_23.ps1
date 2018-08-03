cfba856f4b62b4fc2887e10c7ed35c493ca3c727
csmith783 <31111470+csmith783@users.noreply.github.com>
2018-08-03 07:56:25 -0500

    Chapter 23 Solutions
    Creating Custom Enpoints for PowerShell Remoting.

---
+++ Chapter_23.ps1
@@ -0,0 +1,9 @@
+﻿# Create a remoting endpoint on your localhost named TestEndpoint. COnfigure to load SMBShare Module atuomatically. 
+# Only allow the get-smbshare cmdlet to be visible. Ensure key cmdlets are availible like Exit-pssession
+New-PSSessionConfigurationFile -Path TestEndpoint.pssc -SessionType RestrictedRemoteServer -ModulesToImport SmbShare -VisibleCmdlets Get-SmbShare -Description "Restricted SMB Share Endpoint."
+
+# Register the Configuration file
+Register-PSSessionConfiguration -Path TestEndpoint.pssc -Name TestEndpoint
+
+# Enter the session
+Enter-PSSession -ComputerName LocalHost -ConfigurationName TestEndpoint
